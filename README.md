69bf6b5bb8e260249d49cf1140d23b39b89e1528
iKettles <iain@framer.com>
2020-08-20 14:20:20 +0200

    Initial publish

---
+++ README.md
@@ -0,0 +1,1 @@
+Allows you to integrate data from an HTTP API, Airtable, JSON/TSV/CSV files into your prototype and render design components based on that data.
--- code/canvas.tsx
+++
@@ -1,4 +0,0 @@
-// WARNING: this file is auto generated, any changes will be lost
-import { createDesignComponent, CanvasStore } from "framer"
-const canvas = CanvasStore.shared(); // CANVAS_DATA;
-export const Component = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,email?:string,first_name?:string,last_name?:string,avatar?:string}>(canvas, "id_J6234CJKB", {email:"string",first_name:"string",last_name:"string",avatar:"image"}, 482,153);
