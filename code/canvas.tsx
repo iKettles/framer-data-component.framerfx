08ad6b0a873a85ee0e10b9e87af5e064a0eb9ad5
iKettles <iain@framer.com>
2020-09-01 17:03:11 +0200

    Update canvas

--- code/canvas.tsx
+++ code/canvas.tsx
@@ -1,4 +1,4 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
-export const ListItem = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,avatar?:string,first_name?:string,last_name?:string,email?:string}>(canvas, "id_DMHGHMu1P", {avatar:"image",first_name:"string",last_name:"string",email:"string"}, 445,100);
+export const ListItem = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,avatar?:string,first_name?:string,last_name?:string,email?:string}>(canvas, "id_DMHGHMu1P", {avatar:"image",first_name:"string",last_name:"string",email:"string"}, 287,100);
