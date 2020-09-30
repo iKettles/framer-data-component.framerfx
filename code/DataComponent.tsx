4c61d43f5829a89acfb0e9729b5711ff8efc4fba
iKettles <iain@framer.com>
2020-09-30 13:23:58 +0200

    Use result ID for rendered list item ID

--- code/DataComponent.tsx
+++ code/DataComponent.tsx
@@ -125,7 +125,7 @@ export function DataComponent(props: DataComponentProps) {
                     acc[key] = String(result[key])
                     return acc
                 }, {}),
-                id: `${connectedListItemProps.props.id}—${index}`,
+                id: `${result.id}—${index}`,
                 onTap() {
                     onItemTap(result)
                 },
